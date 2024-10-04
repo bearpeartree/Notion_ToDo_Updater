923e5d284372fb677d043fd6d26f596f8727d511
Lisa <tranlisa@live.de>
2024-10-04 22:55:58 +0200

    Set up connection to page

--- main.py
+++ main.py
@@ -8,9 +8,23 @@ load_dotenv()
 api_key = os.getenv("NOTION_KEY")
 page_id = os.getenv("NOTION_PAGE_ID")
 
-def main():
-    base_url = "https://api.notion.com"
+# Header for each HTTP-Query
+header = {
+    "Authorization": "Bearer " + api_key,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
+}
 
+def main():
+    base_url = "https://api.notion.com/v1/pages/"+ page_id
+
+    response = requests.get(base_url, headers=header)
+    print(response.status_code)
+    print_readable_json(response)
+
+
+def print_readable_json(json_object):
+    print(json.dumps(json_object.json(), indent=2))
 
 
 if __name__ == "__main__":
