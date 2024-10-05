06bb01f336450e8e7db093032444700c24b107a3
Lisa <tranlisa@live.de>
2024-10-05 17:58:38 +0200

    Add playground with examples

--- main.py
+++ main.py
@@ -22,6 +22,55 @@ def main():
     print(response.status_code)
     print_readable_json(response)
 
+    # Playground, Add new page to existing page
+    # page_body = {
+    #     "parent": { "page_id": page_id },
+    #     "properties": {
+    #         "title": {
+    #       "title": [{ "type": "text", "text": { "content": "Hello World!" } }]
+    #         }
+    #     },
+    #     "children": [
+    #     {
+    #       "object": "block",
+    #       "type": "paragraph",
+    #       "paragraph": {
+    #         "rich_text": [{ "type": "text", "text": { "content": "This page was made using an APi call!" } }]
+    #       }
+    #     }
+    #   ]
+    # }
+    # Add Kale-Description via API
+    page_body = {
+        "children": [
+		{
+			"object": "block",
+			"type": "heading_2",
+			"heading_2": {
+				"rich_text": [{ "type": "text", "text": { "content": "Lacinato kale" } }]
+			}
+		},
+		{
+			"object": "block",
+			"type": "paragraph",
+			"paragraph": {
+				"rich_text": [
+					{
+						"type": "text",
+						"text": {
+							"content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+							"link": { "url": "https://en.wikipedia.org/wiki/Lacinato_kale" }
+						}
+					}
+				]
+			}
+		}
+	]
+    }
+
+    new_page_resp = requests.patch(f"https://api.notion.com/v1/blocks/{page_id}/children", json=page_body, headers=header)
+    print_readable_json(new_page_resp)
+
 
 def print_readable_json(json_object):
     print(json.dumps(json_object.json(), indent=2))
