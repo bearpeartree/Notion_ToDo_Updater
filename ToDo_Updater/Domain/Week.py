cf88be667cb3569278567f316ef9bb7abe9097ed
Lisa <tranlisa@live.de>
2024-10-06 22:55:30 +0200

    Add get_days

--- ToDo_Updater/Domain/Week.py
+++ ToDo_Updater/Domain/Week.py
@@ -1,14 +1,14 @@
 from .Day import Day
 
 class Week:
-    def __init__(self, calendar_week, month):
-        self.days = [Day("Montag", calendar_week), 
-                    Day("Dienstag", calendar_week),
-                    Day("Mittwoch", calendar_week),
-                    Day("Donnerstag", calendar_week),
-                    Day("Freitag", calendar_week),
-                    Day("Samstag", calendar_week),
-                    Day("Sonntag", calendar_week)]
+    def __init__(self, calendar_week, month, year):
+        self.days = [Day("Montag", calendar_week, year), 
+                    Day("Dienstag", calendar_week, year),
+                    Day("Mittwoch", calendar_week, year),
+                    Day("Donnerstag", calendar_week, year),
+                    Day("Freitag", calendar_week, year),
+                    Day("Samstag", calendar_week, year),
+                    Day("Sonntag", calendar_week, year)]
         if calendar_week >= 1 and calendar_week <= 52:
             self.calendar_week = calendar_week
         else:
@@ -22,10 +22,14 @@ class Week:
 
     def get_day(self, day_name):
         for i in self.days:
-            if i.get_day() == day_name:
+            if i.get_day_name() == day_name:
                 return i
         raise ValueError("Der Tag existiert nicht.")
     
 
     def get_month(self):
         return self.month
+    
+
+    def get_days(self):
+        return self.days
