0f402f3188a09eabfecb58434543e818799658a7
Lisa <tranlisa@live.de>
2024-10-06 16:56:52 +0200

    Add edge case for test day

--- ToDo_Updater/Domain/Day.py
+++ ToDo_Updater/Domain/Day.py
@@ -1,14 +1,14 @@
 from datetime import date
-from . import Task
+from .Task import Task
 
 class Day:
-    def __init__(self, day, in_week_cal):
-        self.day = day
+    def __init__(self, day_name, in_week_cal):
+        self.day_name = day_name
         self.in_week_cal = in_week_cal
         self.tasks = []
 
-    def get_day(self):
-        return self.day
+    def get_day_name(self):
+        return self.day_name
     
     def get_in_week_cal(self):
         return self.get_in_week_cal
@@ -17,4 +17,7 @@ class Day:
         return self.tasks
     
     def add_task(self, task):
-        self.tasks.append(task)
+        if task != "":
+            self.tasks.append(task)
+        else:
+            raise ValueError("Der Name der Aufgabe darf nicht leer sein!")
--- ToDo_Updater/Tests/test_day.py
+++ ToDo_Updater/Tests/test_day.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pytest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from Domain import Day as day
@@ -11,3 +12,10 @@ def test_add_task():
     monday = day.Day("Montag", 52)
     monday.add_task("Zimmer dekorieren")
     assert monday.get_tasks() == ["Zimmer dekorieren"]
+
+
+def test_add_noname_task():
+    new_week = week.Week(1, "Januar")
+    monday = new_week.get_day("Montag")
+    with pytest.raises(ValueError):
+        monday.add_task("")
