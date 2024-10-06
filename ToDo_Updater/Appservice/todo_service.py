a2f61c1a5f3fd90b89b7b6b49b9ae5e6d24ea437
Lisa <tranlisa@live.de>
2024-10-06 22:29:15 +0200

    Add create_new_week

---
+++ ToDo_Updater/Appservice/todo_service.py
@@ -0,0 +1,9 @@
+from Domain import Week as week
+
+class todo_service: # Will be used by API_Client
+    def __init__(self):
+        self.todo_service = todo_service
+
+    
+    def create_new_week(self, c_week, month):
+        return week.Week(c_week, month)
---
+++ ToDo_Updater/Tests/test_todo_service.py
@@ -0,0 +1,11 @@
+import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Appservice import todo_service as ts
+
+def test_valid_creation_new_week():
+    service = ts.todo_service()
+    new_week = service.create_new_week(52, "Dezember")
+    assert new_week is not None
