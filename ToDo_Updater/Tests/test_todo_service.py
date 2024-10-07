fad6f1718982c49ff98452e24f7959270a8b8158
Lisa <tranlisa@live.de>
2024-10-07 21:37:23 +0200

    Add edge case

--- ToDo_Updater/Tests/test_todo_service.py
+++ ToDo_Updater/Tests/test_todo_service.py
@@ -4,6 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from Appservice import todo_service as ts
+from Domain.Exceptions import IllegalCalendarWeek as icw
 
 def test_valid_creation_new_week():
     service = ts.todo_service()
@@ -15,3 +16,9 @@ def test_week_seven_days():
     service = ts.todo_service()
     new_week = service.create_new_week(52, "Dezember", 2024)
     assert len(new_week.get_days()) == 7
+
+
+def test_invalid_week_creation():
+    service = ts.todo_service()
+    with pytest.raises(icw.IllegalCalendarWeekError):
+        service.create_new_week(53, "Dezember", 2024)
