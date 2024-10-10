6604aef859732b03cf317a8ace0031dfcfb3ee94
Lisa <tranlisa@live.de>
2024-10-10 13:57:00 +0200

    Add date conversion

--- ToDo_Updater/Appservice/todo_service.py
+++ ToDo_Updater/Appservice/todo_service.py
@@ -1,20 +1,39 @@
 from Domain import Week as week
 from Domain import Day as day
 from Domain import Task as task
+import datetime
 
 class todo_service: # Will be used by API_Client
     def __init__(self):
         self.todo_service = todo_service
+        # in mem storage: dict Key = calendar_week, value = week object
+        self.weeks_in_store = {}
 
     # expects the date of the new starting week
     def create_new_week(self, day_of_startweek, month_of_startweek, year_of_startweek):
-        return week.Week(day_of_startweek, month_of_startweek, year_of_startweek)
+        new_week = week.Week(day_of_startweek, month_of_startweek, year_of_startweek)
+        self.weeks_in_store[new_week.get_week_calendar] = new_week
+        return new_week
+    
+
+    def get_stored_weeks(self):
+        return self.weeks_in_store
     
 
     # suppose current_date is a string
-    # But do I need to store it somehwere? -> Just for one session is enough! It does not have persist
+    # But do I need to store it somehwere? -> Just for one session is enough! It does not have persist 
+        # I can retrieve that from Notion itself via API (GET)
+        # Then the API Client can look it up under which week the current date falls 
+        # Idea now: Every start: Fetch from Notion, load onto dict
     def add_task_to_day(task_name, current_date):
-         # find the week in which the current date is => Calculate the 
+         # find the week in which the current date is -> get calendar week of the date
          # take based on the day the suitable date object
          # add the task to the date
+         #current_date_obj = convert_to_date(current_date)
          pass
+    
+    
+    # tag, monat, jahr
+    def convert_to_date(self, some_date):
+        splitted_date = some_date.split(sep=".")
+        return datetime.datetime(int(splitted_date[2]), int(splitted_date[1]), int(splitted_date[0]))
--- ToDo_Updater/Tests/test_todo_service.py
+++ ToDo_Updater/Tests/test_todo_service.py
@@ -5,20 +5,26 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from Appservice import todo_service as ts
 from Domain.Exceptions import IllegalCalendarWeek as icw
+import datetime
+from datetime import date
 
 def test_valid_creation_new_week():
     service = ts.todo_service()
-    new_week = service.create_new_week(7, 10, 2024)
-    assert new_week is not None
+    service.create_new_week(7, 10, 2024)
+    assert len(service.weeks_in_store) == 1
 
 
-def test_week_seven_days():
+def test_seven_days_new_week():
     service = ts.todo_service()
     new_week = service.create_new_week(7, 10, 2024)
     assert len(new_week.get_days()) == 7
 
 
-def test_invalid_week_creation():
+def test_correct_day_conv():
     service = ts.todo_service()
-    with pytest.raises(ValueError):
-        service.create_new_week(60, 100, 3)
+    a_date = service.convert_to_date("6.12.2022")
+    assert a_date.date().month == 12
+    assert a_date.date().day == 6
+    assert a_date.date().year == 2022
+
+    
