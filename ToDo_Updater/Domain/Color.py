2b71341b39608d08b1282226496b2e73e4bb08ed
Lisa <tranlisa@live.de>
2025-01-05 17:00:21 +0100

    Add Color enum

---
+++ ToDo_Updater/Domain/Color.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class color(Enum):
+    BLUE_BACKGROUND = "blue_background"
+    BROWN_BACKGROUND = "brown_background"
+    GRAY_BACKGROUND = "gray_background"
+    GREEN_BACKGROUND = "green_background"
+    ORANGE_BACKGROUND = "orange_background"
+    PINK_BACKGROUND = "pink_background"
+    PURPLE_BACKGROUND = "purple_background"
+    RED_BACKGROUND = "red_background"
+    YELLOW_BACKGROUND = "yellow_background"
