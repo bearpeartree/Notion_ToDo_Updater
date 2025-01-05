effa7d18fa4bd9f317fa501cbe0847ab24d15ec7
Lisa <tranlisa@live.de>
2025-01-05 17:00:43 +0100

    Add json builder

--- ToDo_Updater/Infrastructure/json_builder.py
+++ ToDo_Updater/Infrastructure/json_builder.py
@@ -1,6 +1,15 @@
+from Domain.Color import color
+from random import randint
+
 class json_builder:
     def __init__(self):
         self.json_builder = json_builder
+
+    
+    def __get_random_background_color(self):
+        bc_list = [color_member for color_member in color]
+        rand_index = randint(0, len(bc_list))
+        return bc_list[rand_index]
 
 
     # string - Wochentag (Montag...), Datum
@@ -25,4 +34,32 @@ class json_builder:
                     }]
                 }
             }
-    
+
+
+    # string - Kalenderwoche, Startdatum, Enddatum
+    def build_week_text(self, calendar_week, start_week, end_week):
+
+        random_background_color = self.__get_random_background_color()
+
+        return {
+            "object": "block",
+			"type": "paragraph",
+			"paragraph": {
+				"rich_text": [
+					{
+						"type": "text",
+						"text": {
+							"content": "Woche " + calendar_week + " / " + start_week + " - " + end_week,
+                            "link": None
+						},
+                        "annotations": {
+                            "bold": True,
+        		            "italic": False,
+        		            "strikethrough": False,
+        		            "underline": True,
+        		            "color": random_background_color.value
+                        }
+					}
+				]
+			}
+        }
