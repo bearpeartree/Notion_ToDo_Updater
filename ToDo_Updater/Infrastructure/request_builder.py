cf55393570e51c06dd6b7237cde64694b3ef35f3
Lisa <tranlisa@live.de>
2025-02-06 16:42:00 +0100

    Add playground request builder

--- ToDo_Updater/Infrastructure/request_builder.py
+++ ToDo_Updater/Infrastructure/request_builder.py
@@ -1,5 +1,7 @@
 import json_builder as j
 
+# Hmmm ich will auch mal so wirklich testen (also wie ich es kenne so eine controller klasse oder so)
+
 def main():
     jb = j.json_builder()
     new_json = jb.build_new_day_toggle("Montag", "06.12.2021")
@@ -12,6 +14,10 @@ def main():
     print()
     new_json_weektext = jb.build_week_text("02", "06.01.2025", "12.01.2025")
     print(new_json_weektext)
+
+    print()
+    new_json_week = jb.build_new_week("02")
+    print(new_json_week)
 
 
 
