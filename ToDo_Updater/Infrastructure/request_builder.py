34ac1fd4691c4f71d713440065ca50e83f98535e
Lisa <tranlisa@live.de>
2025-02-07 11:55:27 +0100

    Add more playgrounds

--- ToDo_Updater/Infrastructure/request_builder.py
+++ ToDo_Updater/Infrastructure/request_builder.py
@@ -1,4 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import json_builder as j
+from Appservice import todo_service as ts
 
 # Hmmm ich will auch mal so wirklich testen (also wie ich es kenne so eine controller klasse oder so)
 
@@ -16,9 +21,17 @@ def main():
     print(new_json_weektext)
 
 
+    # print()
+    # new_json_toggle_day = jb.build_new_day_toggle("Montag","1999.12.2021")
+    # print(new_json_toggle_day)
+
     print()
-    new_json_toggle_day = jb.build_new_day_toggle("Montag","1999.12.2021")
-    print(new_json_toggle_day)
+    # todo_s = ts.todo_service()
+    # todo_s.create_new_week(3, 2, 2025)
+    # # print(todo_s.get_stored_weeks())
+    # new_json_week = jb.build_new_week("6")
+    # print(new_json_week)
+
 
 
 if __name__ == "__main__":
