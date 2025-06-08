225c61a0bed4d9c12af256f40681146e144076c9
Lisa <tranlisa@live.de>
2025-06-08 12:47:14 +0200

    Add subtasks

--- ToDo_Updater/Domain/Task.py
+++ ToDo_Updater/Domain/Task.py
@@ -3,6 +3,7 @@ class Task:
         self.task_name = task_name
         self.associated_day = associated_day
         self.finished = False
+        self.sub_tasks = []
 
     def get_task_name(self):
         return self.task_name
@@ -15,3 +16,11 @@ class Task:
     
     def set_task_to_finished(self):
         self.finished = True
+
+    # subtask hinzufügen
+    def add_subtask(self,name_subtask):
+        self.sub_tasks.append(name_subtask)
+
+    # Liste an subtasks zurückgeben
+    def return_subtasks(self):
+        return self.sub_tasks
